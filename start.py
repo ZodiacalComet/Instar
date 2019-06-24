cecc436849702d1f976c2ae929bb5330353e163e
ZodiacalComet <ZodiacalComet@gmail.com>
2019-06-24 17:22:17 -0400

    Simple ping command test

--- start.py
+++ start.py
@@ -4,10 +4,21 @@ import asyncio
 
 from config import instar_token
 
+class GameCog(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send("Pong!")
+
+
 client = commands.Bot(command_prefix="i!", case_insensitive=True)
 
 @client.event
 async def on_ready():
     print("Connected and ready to play!")
+
+client.add_cog(GameCog(client))
 
 client.run(instar_token)
