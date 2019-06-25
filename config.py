9c759389341c3f8350cc188553594a80fba7736d
ZodiacalComet <ZodiacalComet@gmail.com>
2019-06-24 20:36:50 -0400

    Prepare server sequence
    > Create roles, category and channels for the game if not found
    > Added reset command

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 import json
 
+# Getting the token from json file
 with open("./token.json", "r") as f:
     json_data = json.load(f)
 
 instar_token = json_data["token"]
+
+# Uno Config
+category_name = "UNO | Game"
+
+seat_amount = 6
+channel_names = [f"uno-seat-{numb}" for numb in range(1, seat_amount + 1)]
+role_names = [f"{numb} | UNO Seat Access" for numb in range(1, seat_amount + 1)]
--- start.py
+++ start.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 
-from config import instar_token
+from config import instar_token, category_name, seat_amount, channel_names, role_names
 
 class GameCog(commands.Cog):
     def __init__(self, client):
@@ -26,9 +26,67 @@ class GameCog(commands.Cog):
 
     @_start.before_invoke
     async def prepare_server(self, ctx):
+
+        if category_name in [c.name for c in ctx.guild.categories]:
+            return
+
+        guild_roles = ctx.guild.roles
+        game_roles = []
+
+        for role in role_names:
+            for i in range(0, len(guild_roles)):
+                if role == guild_roles[i].name:
+                    await guild_roles[i].delete()
+                    break
+            
+            game_roles.append( await ctx.guild.create_role(name=role) )
+
         await ctx.send("[Prepare server]")
 
-client = commands.Bot(command_prefix="i!", case_insensitive=True)
+        perms = {
+            ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            ctx.guild.me: discord.PermissionOverwrite(read_messages=True)
+        }
+        
+        game_category = await ctx.guild.create_category_channel(category_name, overwrites=perms, reason="For the game of UNO.")
+
+        for i in range(0, seat_amount):
+            channel_perms = {
+                ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+                ctx.guild.me: discord.PermissionOverwrite(read_messages=True),
+                game_roles[i]: discord.PermissionOverwrite(read_messages=True)
+            }
+
+            await game_category.create_text_channel(channel_names[i], overwrites=channel_perms)
+
+    @uno.command(name="reset")
+    async def _reset(self, ctx):
+        guild_roles = ctx.guild.roles
+
+        for role in role_names:
+            for i in range(0, len(guild_roles)):
+                if role == guild_roles[i].name:
+                    await guild_roles[i].delete(reason="Reset")
+                    break
+
+        guild_channels = ctx.guild.text_channels
+
+        for channel in channel_names:
+            for i in range(0, len(guild_channels)):
+                if channel == guild_channels[i].name:
+                    await guild_channels[i].delete(reason="Reset")
+                    break
+
+        guild_categories = ctx.guild.categories
+
+        for i in range(0, len(guild_categories)):
+            if category_name == guild_categories[i].name:
+                await guild_categories[i].delete(reason="Reset")
+                break
+
+        await ctx.send("Everything uno-related has been deleted!")
+
+client = commands.Bot(command_prefix="i.", case_insensitive=True)
 
 @client.event
 async def on_ready():
